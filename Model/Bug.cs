9ddf5d305aa8c8a9430e9a8602fb540b28294af5
Vocal <vocalnight@protonmail.com>
2024-07-20 23:51:15 -0300

    Added the model and viewModel

---
+++ Model/Bug.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatAteBug.Model
+{
+    internal class Bug
+    {
+        public bool IsActive { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public DateOnly AddedDate { get; set; }
+        public DateOnly ResolvedDate { get; set; }
+
+    }
+}
---
+++ ViewModels/BugViewModel.cs
@@ -0,0 +1,52 @@
+﻿using CatAteBug.Model;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatAteBug.ViewModels
+{
+    internal partial class BugViewModel : ViewModelBase
+    {
+
+        public BugViewModel() { }
+
+        public BugViewModel(Bug bug)
+        {
+            IsActive = bug.IsActive;
+            Name = bug.Name;
+            Description = bug.Description;
+            AddedDate = bug.AddedDate;
+            ResolvedDate = bug.ResolvedDate;
+        }
+
+        [ObservableProperty]
+        private bool _IsActive;
+
+        [ObservableProperty]
+        private string _Name;
+
+        [ObservableProperty]
+        private string _Description;
+
+        [ObservableProperty]
+        private DateOnly _AddedDate;
+
+        [ObservableProperty]
+        private DateOnly _ResolvedDate;
+
+        public Bug GetBug()
+        {
+            return new Bug() {
+            IsActive = this.IsActive,
+            Name = this.Name,
+            Description = this.Description,
+            AddedDate = this.AddedDate,
+            ResolvedDate = this.ResolvedDate
+        };
+        }
+
+    }
+}
